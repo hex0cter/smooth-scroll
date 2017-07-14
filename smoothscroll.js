164e362b323d544c0192daa7626c3da15a2a7af7
Daniel Han <hex0cter@gmail.com>
2017-07-14 22:58:02 +0200

    version 1.3, more logging, support capital letters

--- smoothscroll.js
+++ smoothscroll.js
@@ -2,55 +2,63 @@
 var delay = 1; //delay in milliseconds
 var timer = null;
 
-function startScrollUp() {
-  console.log("startScrollUp.")
+function scrollTo(x, y) {
+  var maxWidth = document.body.scrollWidth;
+  var maxHeight = document.body.scrollHeight;
 
+  if (x < 0 || y < 0 || x > maxWidth || y > maxHeight) {
+    return;
+  }
+
+  console.log("scoll to x = " + x + ", y = " + y);
+  window.scroll(x, y);
+}
+
+function startScrollUp() {
   var y = document.scrollingElement.scrollTop;
   var x = document.scrollingElement.scrollLeft;
   if (timer == null) {
+    console.log("startScrollUp.")
     timer = setInterval(function() {
       y = y - parseInt(scrollSettings.scrollSpeed);
-      window.scroll(x, y);
+      scrollTo(x, y);
     }, delay);
   }
 
 }
 
 function startScrollDown() {
-  console.log("startScrollDown.")
-
   var y = document.scrollingElement.scrollTop;
   var x = document.scrollingElement.scrollLeft;
   if (timer == null) {
+    console.log("startScrollDown.")
     timer = setInterval(function() {
       y = y + parseInt(scrollSettings.scrollSpeed);
-      window.scroll(x, y);
+      scrollTo(x, y);
     }, delay);
   }
 }
 
 function startScrollLeft() {
-  console.log("startScrollLeft.")
-
   var y = document.scrollingElement.scrollTop;
   var x = document.scrollingElement.scrollLeft;
   if (timer == null) {
+    console.log("startScrollLeft.")
     timer = setInterval(function() {
       x = x - parseInt(scrollSettings.scrollSpeed);
-      window.scroll(x, y);
+      scrollTo(x, y);
     }, delay);
   }
 }
 
 function startScrollRight() {
-  console.log("startScrollRight.")
-
   var y = document.scrollingElement.scrollTop;
   var x = document.scrollingElement.scrollLeft;
   if (timer == null) {
+    console.log("startScrollRight.")
     timer = setInterval(function() {
       x = x + parseInt(scrollSettings.scrollSpeed);
-      window.scroll(x, y);
+      scrollTo(x, y);
     }, delay);
   }
 }
@@ -83,15 +91,19 @@ function keydownTriggered(e) {
   if (scrollSettings.useVim) {
     switch (e.key) {
       case 'j':
+      case 'J':
         startScrollDown();
         return;
       case 'k':
+      case 'K':
         startScrollUp();
         return;
       case 'h':
+      case 'H':
         startScrollLeft();
         return;
       case 'l':
+      case 'L':
         startScrollRight();
         return;
       default:
@@ -101,24 +113,33 @@ function keydownTriggered(e) {
   if (scrollSettings.useGamer) {
     switch (e.key) {
       case 's':
+      case 'S':
         startScrollDown();
         return;
       case 'w':
+      case 'W':
         startScrollUp();
         return;
       case 'a':
+      case 'A':
         startScrollLeft();
         return;
       case 'd':
+      case 'D':
         startScrollRight();
         return;
       default:
     }
   }
-  console.log("nothing to do.")
+  console.log("Not an interested key press: " + e.key);
+}
+
+function stopScrolling() {
+  console.log("stop scrolling.")
+  clearInterval(timer);
+  timer = null;
 }
 
 function keyupTriggered(e) {
-  clearInterval(timer);
-  timer = null;
+  stopScrolling();
 }
